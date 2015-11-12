675fec68ab09c9fd80506b7821c6559115197058
Emily Blanchard <emblou2@aol.com>
2015-11-12 13:54:58 -0700

    switched from count to start_with

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -18,20 +18,19 @@ class Chisel
   end
 
   def header_parser(input)
-    symbol_count = "#"
-    if symbol_count == 5
-      input.gsub("##### ", "<h5>")
-    elsif symbol_count == 4
-      input.gsub("#### ", "<h4>")
-    elsif symbol_count == 3
-      input.gsub("### ", "<h3>") + " </h#{count}>" #This
+    if input.start_with?("#####")
+      input.gsub("#####", "<h5>")
+    elsif input.start_with?("####")
+      input.gsub("####", "<h4>")
+    elsif input.start_with?("###")
+      input.gsub("###", "<h3>") + " </h#{count}>" #This
       #is not working, not ready to give up this idea
       #also thinking about how to make symbol_count times
       #equal to header.
-    elsif symbol_count == 2
-      input.gsub("## ", "<h2>")
-    elsif symbol_count == 1
-      input.gsub("# ", "<h1>")
+    elsif input.start_with?("##")
+      input.gsub("##", "<h2>")
+    else #input.start_with?("#")
+      input.gsub("#", "<h1>")
     end
   end
 
