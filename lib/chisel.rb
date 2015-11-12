5b65c990897a4aa92dc8f1ed4c7d26258c7509cc
Emily Blanchard <emblou2@aol.com>
2015-11-12 13:21:10 -0700

    need the end tag, not working yet

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -24,7 +24,10 @@ class Chisel
     elsif symbol_count == 4
       input.gsub("#### ", "<h4>")
     elsif symbol_count == 3
-      input.gsub("### ", "<h3>")
+      input.gsub("### ", "<h3>") + " </h#{count}>" #This
+      #is not working, not ready to give up this idea
+      #also thinking about how to make symbol_count times
+      #equal to header.
     elsif symbol_count == 2
       input.gsub("## ", "<h2>")
     elsif symbol_count == 1
