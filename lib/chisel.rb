730ac167fa7e227000173fb3285bca85be84b51a
Emily Blanchard <emblou2@aol.com>
2015-11-15 16:59:29 -0700

    added lists methods

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -16,23 +16,36 @@ class Chisel
     something = input[2..-3]
     "<strong>#{something}</strong>"
   end
+####Don't forget words wrapped in <em> and <strong>
+####can be combined. My *emphasized and **stronged** test* is awesome.
 
   def header_parser(input)
     if input.start_with?("#####")
-      input.gsub("#####", "<h5>")
+      input.gsub("#####", "<h5>") + "</h5>"
     elsif input.start_with?("####")
-      input.gsub("####", "<h4>")
+      input.gsub("####", "<h4>") + "</h4>"
     elsif input.start_with?("###")
-      input.gsub("###", "<h3>") + " </h#{count}>" #This
-      #is not working, not ready to give up this idea
-      #also thinking about how to make symbol_count times
-      #equal to header.
+      input.gsub("###", "<h3>") + "</h3>"
     elsif input.start_with?("##")
-      input.gsub("##", "<h2>")
+      input.gsub("##", "<h2>") + "</h2>"
     else #input.start_with?("#")
-      input.gsub("#", "<h1>")
+      input.gsub("#", "<h1>") + "</h1>"
     end
   end
 
+  def blockquote(input)
+    something = input[1..-1]
+    "<blockquote><p>#{something}</p></blockquote>"
+  end
+
+  def unordered_list(input) ##need map or map with index...maybe?
+    something = input[1..-1]
+    "<ul>\n<li>#{something}</li>\n</ul>"
+  end
+                            ##make it a string maybe? Need to ask!
+  def ordered_list(input)
+    something = input[1..-1]
+    "<ol>\n<li>#{something}</li>\n</ol>"
+  end
 
 end
