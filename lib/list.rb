e3a84923cfcc2c2b2720c219caa41dc068966e47
Emily Blanchard <emblou2@aol.com>
2015-11-17 20:43:08 -0700

    pseudo code added for tomorrow

--- lib/list.rb
+++ lib/list.rb
@@ -1,5 +1,14 @@
 class List
-  def unordered_list(input)
+  def unordered_list(input)#maybe (index, input)
+
+        #maybe split the input subtract 1 for position
+        #like 1st position is zero, 2nd is one, etc...
+        #need to know how many are in the list...map? enum?
+        #for zero "\n<ul>\n<li>#{something}</li>\n"
+        #then  "\n<li>#{something}</li>\n</ul>\n"
+        #then "<li>#{something}</li>"
+        #should be a hash, then can use INDEX for position.
+
     if input == ""
     return ""
     end
@@ -10,7 +19,12 @@ class List
       "<ul><li>#{something}</li></ul>"
   end
 
+
+
+
   def ordered_list(input)
+      ##should match unordered, but with <ol> not <ul>
+      ##this should be able to be refactored once it works.
     if input == ""
     return ""
     end
