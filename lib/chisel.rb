a96ec293da77e40999f82bb5fd6f9735eb4163d9
Emily Blanchard <emblou2@aol.com>
2015-11-18 20:24:54 -0700

    dunno

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,6 +1,10 @@
 class Chisel
 
-
+#Justin meet ideas: need one long string, gets
+#split that string, then do what you will with
+#the parts. Think factory analogy. Need main parser
+#Chunks phrase that makes sense...need for both
+#markdown (input) and html(output)
 
 
 end
